1d7e9086672ca5fd572be5317fad359c944177db
Roshni Yadav <47268468+roshni-1@users.noreply.github.com>
2024-02-08 04:30:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,29 +14,24 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Extract input values from the request
-        data = request.get_json()
+        # Extract input values from the form data
+        credit_score = float(request.form['credit_score'])
+        relationship_duration = float(request.form['relationship_duration'])
+        repayment_tenure = float(request.form['repayment_tenure'])
+        income = float(request.form['income'])
+        investments = float(request.form['investments'])
+        savings_account_balance = float(request.form['savings_account_balance'])
+        outstanding_loan = float(request.form['outstanding_loan'])
 
-        # Validate if all required fields are present
-        if all(key in data for key in ['credit_score', 'relationship_duration', 'repayment_tenure',
-                                       'income', 'investments', 'savings_account_balance', 'outstanding_loan']):
-            # Make a prediction using the loaded model
-            input_data = np.array([[float(data['credit_score']), float(data['relationship_duration']),
-                                    float(data['repayment_tenure']), float(data['income']),
-                                    float(data['investments']), float(data['savings_account_balance']),
-                                    float(data['outstanding_loan'])]])
+        # Make a prediction using the loaded model
+        input_data = np.array([[credit_score, relationship_duration, repayment_tenure, income, investments,
+                                savings_account_balance, outstanding_loan]])
+        prediction = model.predict(input_data)
 
-            prediction = model.predict(input_data)
-
-            # Return the prediction as JSON
-            return jsonify({'prediction': int(prediction[0])})
-        else:
-            # If any required field is missing, return an error
-            return jsonify({'error': 'Missing required fields'}), 400
-
+        # Return the prediction as JSON
+        return jsonify({'prediction': int(prediction[0])})
     except Exception as e:
-        # Handle exceptions and return an error response
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
