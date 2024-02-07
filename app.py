f3e4fc9c80b91be7ede509019e58840d053e3e58
Roshni Yadav <47268468+roshni-1@users.noreply.github.com>
2024-02-08 04:44:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,28 +13,22 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Extract input values from the JSON data
-        data = request.json
-        credit_score = float(data['credit_score'])
-        relationship_duration = float(data['relationship_duration'])
-        repayment_tenure = float(data['repayment_tenure'])
-        income = float(data['income'])
-        investments = float(data['investments'])
-        savings_account_balance = float(data['savings_account_balance'])
-        outstanding_loan = float(data['outstanding_loan'])
+    # Extract input values from the form data
+    credit_score = float(request.form['credit_score'])
+    relationship_duration = float(request.form['relationship_duration'])
+    repayment_tenure = float(request.form['repayment_tenure'])
+    income = float(request.form['income'])
+    investments = float(request.form['investments'])
+    savings_account_balance = float(request.form['savings_account_balance'])
+    outstanding_loan = float(request.form['outstanding_loan'])
 
-        # Make a prediction using the loaded model
-        input_data = np.array([[credit_score, relationship_duration, repayment_tenure, income, investments,
-                                savings_account_balance, outstanding_loan]])
-        prediction = model.predict(input_data)
+    # Make a prediction using the loaded model
+    input_data = np.array([[credit_score, relationship_duration, repayment_tenure, income, investments,
+                            savings_account_balance, outstanding_loan]])
+    prediction = model.predict(input_data)
 
-        # Return the prediction as JSON
-        return jsonify({'prediction': int(prediction[0])})
-
-    except Exception as e:
-        # Return an error message as JSON
-        return jsonify({'error': str(e)}), 500
+    # Return the prediction as JSON
+    return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
     app.run(debug=True)
