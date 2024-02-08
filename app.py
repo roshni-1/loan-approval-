9f4c3ada232e162828db58e07d2e3b1e382f625e
Roshni Yadav <47268468+roshni-1@users.noreply.github.com>
2024-02-08 09:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-# Load the pre-trained machine learning model
 model = joblib.load('loan_approval_model.joblib')
 
 @app.route('/')
@@ -14,7 +13,6 @@ def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Extract input values from the request
         credit_score = float(request.form['credit_score'])
         relationship_duration = float(request.form['relationship_duration'])
         repayment_tenure = float(request.form['repayment_tenure'])
@@ -23,12 +21,10 @@ def predict():
         savings_account_balance = float(request.form['savings_account_balance'])
         outstanding_loan = float(request.form['outstanding_loan'])
 
-        # Make a prediction using the loaded model
         input_data = np.array([[credit_score, relationship_duration, repayment_tenure, income, investments,
                                 savings_account_balance, outstanding_loan]])
         prediction = model.predict(input_data)
 
-        # Return the prediction as JSON
         return jsonify({'prediction': int(prediction[0])})
 
     except Exception as e:
